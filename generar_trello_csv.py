8756201362511fd27f2d0131fc7a49857bc07ad9
julio1980p <perezduranjulio@gmail.com>
2020-04-30 11:47:55 -0400

    subir archivo a google drive con errores aun

--- generar_trello_csv.py
+++ generar_trello_csv.py
@@ -5,6 +5,7 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from apiclient.http import MediaFileUpload
 #from pydrive.auth import GoogleAuth
 #from pydrive.drive import GoogleDrive
 
@@ -35,7 +36,7 @@ if not creds or not creds.valid:
 
 drive_service = build('drive', 'v3', credentials=creds)
 
-#crea una carpeta drive solo si no existe alguna carpeta con el mismo nombre
+#crea una carpeta drive solo si no existe alguna carpeta con el mismo nombre, retorna el id de la carpeta existente o creada
 def crear_carpeta_drive(carpeta):
 	folder_id_exists = exists_carpeta_drive(carpeta)
 	if not folder_id_exists:
@@ -50,6 +51,8 @@ def crear_carpeta_drive(carpeta):
 
 	else:
 		folder_id = folder_id_exists
+
+	return folder_id
 
 
 #retorna false si la carpeta no existe. De lo contrario retorna el id de la carpeta en el drive
@@ -66,7 +69,23 @@ def exists_carpeta_drive(carpeta):
 		return file.get('id')
 	page_token = response.get('nextPageToken', None)
 	if page_token is None:
-		return false
+		return False
+
+
+#retorna el id del archivo del drive subido
+def subir_file(file,carpeta_dest):
+	folder_id = crear_carpeta_drive(carpeta_dest)
+	file_metadata = {
+	    'name': file,
+	    'parents': [folder_id]
+	}
+	media = MediaFileUpload(file,mimetype=None,resumable=True)
+
+	file = drive_service.files().create(body=file_metadata,media_body=media,fields='id').execute()
+	print ('File ID: %s' % file.get('id'))
+	return file.get('id')
+#retorna el id del archivo del drive subido
+
 
 	
 
@@ -74,9 +93,10 @@ def exists_carpeta_drive(carpeta):
 
 if __name__ == '__main__':
 	try:
-		file_io_base = open('trello_data_api.json','wb')
+		file_io_base = open('trello_data_api.csv','wb')
 	#download_file_google_drive(sys.argv[1],file_io_base)
-		crear_carpeta_drive("hola")
+		crear_carpeta_drive("Trello_Data")
+		subir_file('trello_data_api.csv',"Trello_Data")
 	#procesar_json()
 
 	except IndexError:
